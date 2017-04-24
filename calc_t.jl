455d3650e58c4182a42392f292cbf02673be13b2
lionx <nbs5044@psu.edu>
2017-04-24 12:16:06 -0400

    changed how E_cr and sn_frac are passed to get_T and changed result file name

--- calc_t.jl
+++ calc_t.jl
@@ -9,15 +9,16 @@ using StatsBase
 @everywhere get_T(1e52,1.0);
 n_exp =1000
 
+my_E_cr = 1e52; 
+my_sn_frac = 1.0; 
+
+
 my_ts = SharedArray(Float64,n_exp,len_sne+1);
 #tic()
 @profile @sync @parallel for n in 1:n_exp
-       # println("hello")
-        t_opt_T = get_T(1e52,1.0);
+        t_opt_T = get_T(my_E_cr,my_sn_frac);
         my_ts[n,1:end-1] = t_opt_T[1];
         my_ts[n,end] = t_opt_T[2];
-       # println("goodbye");
 end
 #toc();
-#rmprocs(workers())
-writedlm(string("results/T_w_sig_",n_exp,".dat"),my_ts);
+writedlm(string("results/T_",n_exp,"_",my_E_cr,"_",my_sn_frac,".dat"),my_ts);
