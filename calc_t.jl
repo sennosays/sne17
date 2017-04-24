cd043d731de127b7d3c98747a2b610daff4808d8
Hephaestus <nicholas.senno@gmail.com>
2017-04-24 12:22:45 -0400

    @time instead of @profile

--- calc_t.jl
+++ calc_t.jl
@@ -7,7 +7,7 @@ using StatsBase
 
 @everywhere include("sne_init.jl");
 @everywhere get_T(1e52,1.0);
-n_exp =1000
+n_exp = 5
 
 my_E_cr = 1e52; 
 my_sn_frac = 1.0; 
@@ -15,7 +15,7 @@ my_sn_frac = 1.0;
 
 my_ts = SharedArray(Float64,n_exp,len_sne+1);
 #tic()
-@profile @sync @parallel for n in 1:n_exp
+@time @sync @parallel for n in 1:n_exp
         t_opt_T = get_T(my_E_cr,my_sn_frac);
         my_ts[n,1:end-1] = t_opt_T[1];
         my_ts[n,end] = t_opt_T[2];
