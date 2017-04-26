efccd2cea91948a264bce1d95deba35770a8aa42
Hephaestus <nicholas.senno@gmail.com>
2017-04-25 23:58:45 -0400

    clean up before trip

--- calc_t.jl
+++ calc_t.jl
@@ -9,14 +9,15 @@ using StatsBase
 @everywhere get_T(1e52,1.0);
 n_exp = 10000;
 
-my_E_cr = 1e46; 
+my_E_cr = 1e52; 
 my_sn_frac = 1.0; 
 
 
 my_ts = SharedArray(Float64,n_exp,len_sne+1);
 #tic()
 @time @sync @parallel for n in 1:n_exp
-        t_opt_T = get_T(my_E_cr,my_sn_frac);
+	println(n);
+	t_opt_T = get_T(my_E_cr,my_sn_frac);
         my_ts[n,1:end-1] = t_opt_T[1];
         my_ts[n,end] = t_opt_T[2];
 end
