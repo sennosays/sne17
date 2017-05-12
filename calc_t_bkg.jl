49d883fc70a869eca5e1bbf20902044c3732edf1
Hephaestus <nicholas.senno@gmail.com>
2017-05-12 15:50:29 -0400

    started calc grid

--- calc_t_bkg.jl
+++ calc_t_bkg.jl
@@ -14,9 +14,7 @@ n_exp = 1000;
 my_ts = SharedArray(Float64,n_exp,len_sne+1);
 #tic()
 @time @sync @parallel for n in 1:n_exp
-	t_opt_T = get_T(1e46,0.0);
-        my_ts[n,1:end-1] = t_opt_T[1];
-        my_ts[n,end] = t_opt_T[2];
+	my_ts[n,1:end-1], my_ts[n,end] = get_T(1e46,0.0);
 end
 #toc();
-writedlm(string("results/T_",n_exp,"_46.0_0.0.dat"),my_ts);
+writedlm(string("results/T_",n_exp,"_46.0_0.0_new.dat"),my_ts);
